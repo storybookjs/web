012becf6c5775d6790697d917216797934e74cc6
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-11-30 11:42:31 +0000

    Test with local docs

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,9 +1,3 @@
 export default function Docs() {
-  return (
-    <main className="max-w-8xl mx-auto px-4 lg:px-8 py-12 flex gap-4">
-      <div className="w-[228px] bg-zinc-400/10 dark:bg-zinc-600/10 min-h-[1400px] max-[848px]:hidden" />
-      <div className="w-full flex-1 bg-zinc-400/10 dark:bg-zinc-600/10 min-h-[1400px]" />
-      <div className="w-[228px] bg-zinc-400/10 dark:bg-zinc-600/10 min-h-[1400px] max-[1148px]:hidden" />
-    </main>
-  );
+  return <div>Hello World</div>;
 }
