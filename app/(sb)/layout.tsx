5162feb9188609eb3e092738a4decd5e12d8309b
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-01-11 18:38:26 +0000

    Update layout.tsx

--- app/(sb)/layout.tsx
+++ app/(sb)/layout.tsx
@@ -1,16 +1,6 @@
 import type { PropsWithChildren } from 'react';
 import React from 'react';
 
-export default function RootLayout({ children }: PropsWithChildren<{}>) {
-  return (
-    <html lang="en">
-      <body>
-        <main>
-          <div className="page">
-            <>{children}</>
-          </div>
-        </main>
-      </body>
-    </html>
-  );
+export default function NestedLayout({ children }: PropsWithChildren<{}>) {
+  return <>{children}</>;
 }
