221ac27574efd6725db1520fdac4e9901e646813
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-01-15 09:10:08 +0000

    Add to build

--- app/releases/[slug]/page.tsx
+++ app/releases/[slug]/page.tsx
@@ -34,6 +34,7 @@ export const generateStaticParams = async () => {
 export default async function Page({ params: { slug } }: Props) {
   const releases = getReleases();
 
+  // TODO: This is not really working on prod
   if (releases.includes(slug) === false) return notFound();
 
   const page = await getRelease(slug);
