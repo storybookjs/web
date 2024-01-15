a9c9d0271832c1af09f9a66201fa1140be628d70
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-01-15 18:23:14 +0000

    Delete main.ts

--- .storybook/main.ts
+++
@@ -1,9 +0,0 @@
-import type { StorybookConfig } from '@storybook/nextjs-server';
-
-const config: StorybookConfig = {
-  stories: ['../**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-controls'],
-framework: '@storybook/nextjs-server',
-};
-
-export default config;
