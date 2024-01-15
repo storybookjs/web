6a6b4ea693833e98999e11c0b4554ed53e3e62ca
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-01-15 08:40:38 +0000

    Update page.tsx

--- app/releases/[slug]/page.tsx
+++ app/releases/[slug]/page.tsx
@@ -1,8 +1,9 @@
+import { getRelease } from "@/lib/get-release";
 import fs from "fs";
 
 interface Props {
   params: {
-    slug: string[];
+    slug: string;
   };
 }
 
@@ -19,15 +20,19 @@ export const generateStaticParams = async () => {
 };
 
 export default async function Page({ params: { slug } }: Props) {
+  const page = await getRelease(slug);
   const file = await fs.promises.readFile(
     process.cwd() + "/content/data.json",
     "utf8"
   );
   const data = JSON.parse(file);
 
+  console.log(page);
+
   return (
     <main className="flex flex-col min-h-screen items-center justify-center">
       <div>Slug: {slug}</div>
+      <div>{page?.frontmatter?.title}</div>
       <h1>{data.title}</h1>
       <p>{data.content}</p>
     </main>
