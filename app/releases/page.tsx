06ef8464c4691bc58db39f727c2d65dae690d4fe
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-01-15 09:21:39 +0000

    Create page.tsx

---
+++ app/releases/page.tsx
@@ -0,0 +1,8 @@
+import { getReleases } from "@/lib/get-releases";
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  const releases = getReleases();
+
+  redirect(`/releases/${releases[releases.length - 1]}`);
+}
