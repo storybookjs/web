da4ac44a63db976d082d87ba04a923b678ba5975
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-01-21 22:20:56 +0000

    Fix some layout

--- components/docs/table-of-content.tsx
+++ components/docs/table-of-content.tsx
@@ -29,7 +29,7 @@ export const TableOfContent: FC<TableOfContentProps> = ({ headings }) => {
   const [isInView, setIsInView] = useState<string[]>([]);
 
   return (
-    <nav className="w-[228px] max-[1148px]:hidden block sticky self-start top-[72px]">
+    <nav className="w-[228px] hidden lg:block sticky self-start top-[72px]">
       <ScrollArea className="h-[calc(100vh-72px)] w-full">
         <div className="py-12 ">
           <div className="block text-sm font-bold h-8">On this page</div>
