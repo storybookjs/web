18e112d107f2179f9ea8ebebb2a85364d22d9fe7
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-01-22 12:54:03 +0000

    Fix some light details

--- components/docs/mdx/h2.tsx
+++ components/docs/mdx/h2.tsx
@@ -15,7 +15,7 @@ export const H2 = ({ children, id }: H2Props) => {
       <div id={id} className="absolute -translate-y-24" />
       <a
         href={`#${id}`}
-        className="text-black group-hover:text-blue-500 transition-colors duration-200"
+        className="text-black group-hover:text-blue-600 transition-colors duration-200"
       >
         {children}
         <span className="inline-block ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
--- components/docs/mdx/h3.tsx
+++ components/docs/mdx/h3.tsx
@@ -1,5 +1,5 @@
 import { LinkIcon } from "@storybook/icons";
-import { DetailedHTMLProps, FC, HTMLAttributes, ReactNode } from "react";
+import { DetailedHTMLProps, HTMLAttributes } from "react";
 
 type H3Props = DetailedHTMLProps<
   HTMLAttributes<HTMLHeadingElement>,
@@ -8,11 +8,14 @@ type H3Props = DetailedHTMLProps<
 
 export const H3 = ({ children, id }: H3Props) => {
   return (
-    <h3 className="relative text-xl mt-10 mb-4 font-bold" data-docs-heading>
+    <h3
+      className="relative text-xl mt-10 mb-4 font-bold group"
+      data-docs-heading
+    >
       <div id={id} className="absolute -translate-y-24" />
       <a
         href={`#${id}`}
-        className="text-black group-hover:text-blue-500 transition-colors duration-200"
+        className="text-black group-hover:text-blue-600 transition-colors duration-200"
       >
         {children}
         <span className="inline-block ml-2 opacity-0 group-hover:opacity-100 transition-opacity duration-200">
