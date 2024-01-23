3d2c4b97afaa745d18536b9704f303ac26e7b3c1
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-01-23 12:01:54 +0000

    Update if-renderer.tsx

--- components/docs/mdx/if-renderer.tsx
+++ components/docs/mdx/if-renderer.tsx
@@ -1,9 +1,22 @@
-import { FC } from "react";
+"use client";
 
-type Props = {
-  id: string;
+import { useDocs } from "@/app/docs/provider";
+import { ReactNode } from "react";
+
+type IfRendererProps = {
+  renderer: string | string[];
+  children: ReactNode;
 };
 
-export const IfRenderer: FC<Props> = ({ id }) => {
-  return <div className="">IfRenderer</div>;
+export const IfRenderer = ({ renderer, children }: IfRendererProps) => {
+  const { activeRenderer } = useDocs();
+  const rendererArray = Array.isArray(renderer) ? renderer : [renderer];
+
+  // If there is no active renderer, don't render anything
+  if (activeRenderer === null) return null;
+
+  // If the active renderer is in the array of allowed renderers, render the children
+  if (rendererArray.includes(activeRenderer)) return <>{children}</>;
+
+  return null;
 };
