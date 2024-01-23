67426f2630f0eaa8b5a8d9da6abbc62e94a6b4f5
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-01-23 12:03:49 +0000

    Cleaning

--- components/docs/mdx/details.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactNode } from "react";
-
-interface Props {
-  children?: ReactNode;
-}
-
-export const Details: FC<Props> = ({ children }) => {
-  return <details className="my-10">{children}</details>;
-};
