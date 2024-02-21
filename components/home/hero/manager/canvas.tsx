a7fdda5a7bf04207b96e3b36fd3fa70f614b7727
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-02-21 09:57:10 +0000

    Update canvas.tsx

--- components/home/hero/manager/canvas.tsx
+++ components/home/hero/manager/canvas.tsx
@@ -12,7 +12,7 @@ export const Canvas: FC<{ slide: number }> = ({ slide }) => {
       exit={{ opacity: 0 }}
       className={cn(
         "absolute w-full top-10 h-[calc(60%-40px)] flex items-center justify-center text-black bg-white transition-all px-4",
-        slide === 4 && "lg:w-[calc(100%-320px)] h-[calc(100%-40px)]"
+        slide === 4 && "lg:w-[calc(100%-320px)] lg:h-[calc(100%-40px)]"
       )}
     >
       <Image priority src={timeframe1} alt="TimeFrame" />
