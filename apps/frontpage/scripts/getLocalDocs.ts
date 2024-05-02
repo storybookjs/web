59d980eed08b8172f5d35a249572b9c33e88f2d9
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-05-02 18:02:37 +0100

    Update getLocalDocs.ts

--- apps/frontpage/scripts/getLocalDocs.ts
+++ apps/frontpage/scripts/getLocalDocs.ts
@@ -37,6 +37,8 @@ async function fetchAndExtract(version: DocsVersion) {
 
   const response = await fetch(url || '');
 
+  console.log(response);
+
   if (!response.ok) {
     throw new Error(`unexpected response ${response.statusText}`);
   }
