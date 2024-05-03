6b5b93d3bc8a4737236aa4bf7ca6f3d27c341ba6
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-05-03 22:03:06 +0100

    Bring back the homepage

--- apps/frontpage/next.config.js
+++ apps/frontpage/next.config.js
@@ -1,4 +1,51 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars3.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars2.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars0.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars1.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.opencollective.com',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/docs/get-started',
+        destination: '/docs',
+        permanent: true,
+      },
+    ];
+  },
+  // This was added to fix the error with remarkExpressiveCode
+  // https://stackoverflow.com/questions/77009138/module-has-no-exports-error-works-fine-on-stackblitz-but-fails-locally
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+    return config;
+  },
   transpilePackages: ['@repo/ui', '@repo/utils'],
 };
