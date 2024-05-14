1e71043251ece86aa8862eb27601105d0e8650a9
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-05-14 18:57:57 +0100

    Update index.tsx

--- apps/frontpage/components/docs/mdx/code-snippets/index.tsx
+++ apps/frontpage/components/docs/mdx/code-snippets/index.tsx
@@ -8,23 +8,12 @@ interface LocalProps {
 }
 
 export async function CodeSnippets({ path, activeVersion }: LocalProps) {
-  // This is how files are structured.
-  // [renderer]/[filename].[option].[language].mdx
-  // [renderer]/[filename].[language].mdx
-  //
-  // Options are optional and are serving 2 purposes:
-  // - Create tabs (could be anything)
-  // - Set the package manager (npm | yarn | pnpm)
-  //
-  // option in angular: with-builder | ...
-  // option in Vue: 2 | 3
-  // option in common: could be anything
+  // If there is no path or active version, return null
+  // TODO: Perhaps we could return a message saying that there are no code snippets
+  if (!path || !activeVersion) return null;
 
   // Get metadata for all files from the Code Snippets component
   // This happen on the server since we need to call the file system (fs)
-
-  if (!path || !activeVersion) return null;
-
   const codeSnippetsContent: CodeSnippetsProps[] = await getMetadata({
     path,
     activeVersion,
