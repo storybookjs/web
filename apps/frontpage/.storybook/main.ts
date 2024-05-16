bd9f0b42f28be0004f50808034582a09591a6533
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-05-16 08:33:16 +0100

    Remove onboarding

--- apps/frontpage/.storybook/main.ts
+++ apps/frontpage/.storybook/main.ts
@@ -17,7 +17,6 @@ const config: StorybookConfig = {
     '../components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
-    getAbsolutePath('@storybook/addon-onboarding'),
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@chromatic-com/storybook'),
