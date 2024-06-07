8ced89abe18bd8e1a36111c1da897a8997f0eb47
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-06-07 14:58:08 +0100

    Update p.tsx

--- apps/frontpage/components/docs/mdx/p.tsx
+++ apps/frontpage/components/docs/mdx/p.tsx
@@ -6,7 +6,7 @@ interface PProps {
 
 export const P: FC<PProps> = ({ children }) => {
   return (
-    <p className="text-md mb-6 mt-0 leading-7 [&>a]:text-blue-500">
+    <p className="text-md [&>code]:text-md mb-6 mt-0 leading-7 [&>a]:text-blue-500">
       {children}
     </p>
   );
