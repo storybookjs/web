29575d9d064b3036be403e1e55415856822a78fe
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-06-07 16:37:56 +0100

    Update next.config.js

--- apps/frontpage/next.config.js
+++ apps/frontpage/next.config.js
@@ -40,19 +40,11 @@ module.exports = {
   },
   transpilePackages: ['@repo/ui', '@repo/utils'],
   async redirects() {
+    // Add the wild cards at the bottom of the list
+    // to avoid conflicts with the more specific redirects
     return [
       {
         source: '/docs/get-started',
-        destination: '/docs',
-        permanent: true,
-      },
-      {
-        source: '/basics/:path*',
-        destination: '/docs',
-        permanent: true,
-      },
-      {
-        source: '/docs/basics/:path*',
         destination: '/docs',
         permanent: true,
       },
@@ -76,6 +68,65 @@ module.exports = {
         source: '/docs/basics/guide-react-native',
         destination:
           'https://github.com/storybookjs/react-native#storybook-for-react-native',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/writing-stories',
+        destination: '/docs/get-started/whats-a-story',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/exporting-storybook',
+        destination: '/docs/workflows/publish-storybook',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/faq',
+        destination: '/docs/workflows/faq',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/live-examples',
+        destination:
+          'https://github.com/storybookjs/storybook/blob/next/examples/README.md',
+        permanent: true,
+      },
+      {
+        source: '/docs/examples',
+        destination: '/docs/get-started/examples',
+        permanent: true,
+      },
+      /////////////////////////////////////////////////
+      /////////////// 🐺 Wild Cards ///////////////////
+      /////////////////////////////////////////////////
+      {
+        source: '/basics/:path*',
+        destination: '/docs',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/:path*',
+        destination: '/docs',
+        permanent: true,
+      },
+      {
+        source: '/configurations/:path*',
+        destination: '/docs/configure',
+        permanent: true,
+      },
+      {
+        source: '/examples/:path*',
+        destination: '/docs',
+        permanent: true,
+      },
+      {
+        source: '/logos/:path*',
+        destination: '/docs',
+        permanent: true,
+      },
+      {
+        source: '/testing/:path*',
+        destination: '/docs',
         permanent: true,
       },
     ];
