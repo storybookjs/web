b196ced78a8475528c799d78ff8b967f9069f073
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-06-07 16:09:57 +0100

    Add older riderects

--- apps/frontpage/next.config.js
+++ apps/frontpage/next.config.js
@@ -28,15 +28,6 @@ module.exports = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/docs/get-started',
-        destination: '/docs',
-        permanent: true,
-      },
-    ];
-  },
   // This was added to fix the error with remarkExpressiveCode
   // https://stackoverflow.com/questions/77009138/module-has-no-exports-error-works-fine-on-stackblitz-but-fails-locally
   webpack: (config) => {
@@ -48,4 +39,120 @@ module.exports = {
     return config;
   },
   transpilePackages: ['@repo/ui', '@repo/utils'],
+  async redirects() {
+    return [
+      {
+        source: '/docs/get-started',
+        destination: '/docs',
+        permanent: true,
+      },
+      {
+        source: '/basics/quick-start-guide',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/basics/slow-start-guide',
+        destination: '/docs/configure',
+        permanent: true,
+      },
+      {
+        source: '/basics/guide-react',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/basics/guide-react-native',
+        destination:
+          'https://github.com/storybookjs/react-native#storybook-for-react-native',
+        permanent: true,
+      },
+      {
+        source: '/basics/guide-vue',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/basics/guide-angular',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/basics/guide-mithril',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/basics/guide-ember',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/basics/guide-riot',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/basics/guide-html',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/quick-start-guide',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/slow-start-guide',
+        destination: '/docs/configure',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/slow-start-guide',
+        destination: '/docs/configure',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/guide-react',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/guide-react-native',
+        destination:
+          'https://github.com/storybookjs/react-native#storybook-for-react-native',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/guide-vue',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/guide-angular',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/guide-mithril',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/guide-ember',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/guide-riot',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+      {
+        source: '/docs/basics/guide-html',
+        destination: '/docs/get-started',
+        permanent: true,
+      },
+    ];
+  },
 };
