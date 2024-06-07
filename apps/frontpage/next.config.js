b67a88866da23fac73dd2742b9e3dfbd6dc9efbc
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-06-07 16:53:32 +0100

    Update next.config.js

--- apps/frontpage/next.config.js
+++ apps/frontpage/next.config.js
@@ -48,6 +48,7 @@ module.exports = {
         destination: '/docs',
         permanent: true,
       },
+      /////// Older (prev 5.0 doc URLs) /////////////
       {
         source: '/basics/slow-start-guide',
         destination: '/docs/configure',
@@ -70,6 +71,7 @@ module.exports = {
           'https://github.com/storybookjs/react-native#storybook-for-react-native',
         permanent: true,
       },
+      ///////// Pre 6.0 URLs ///////////////////////
       {
         source: '/docs/basics/writing-stories',
         destination: '/docs/get-started/whats-a-story',
@@ -94,6 +96,160 @@ module.exports = {
       {
         source: '/docs/examples',
         destination: '/docs/get-started/examples',
+        permanent: true,
+      },
+      {
+        source: '/docs/guides/slow-start-guide',
+        destination: '/docs/configure',
+        permanent: true,
+      },
+      {
+        source: '/docs/guides/guide-react-native',
+        destination:
+          'https://github.com/storybookjs/react-native#storybook-for-react-native',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/overview',
+        destination: '/docs/configure',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/options-parameter',
+        destination: '/docs/configure/features-and-behavior',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/default-config',
+        destination: '/docs/configure',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/custom-webpack-config',
+        destination: '/docs/builders/webpack',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/custom-babel-config',
+        destination: '/docs/configure/compilers',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/typescript-config',
+        destination: '/docs/configure/typescript',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/add-custom-head-tags',
+        destination: '/docs/configure/story-rendering#adding-to-head',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/add-custom-body',
+        destination: '/docs/configure/story-rendering#adding-to-body',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/serving-static-files',
+        destination:
+          '/docs/configure/images-and-assets#serving-static-files-via-storybook',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/env-vars',
+        destination: '/docs/configure/environment-variables',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/theming',
+        destination: '/docs/configure/theming',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/cli-options',
+        destination: '/docs/api/cli-options',
+        permanent: true,
+      },
+      {
+        source: '/docs/configurations/standalone-options',
+        destination:
+          'https://github.com/storybookjs/storybook/blob/next/lib/core/docs/standalone.md',
+        permanent: true,
+      },
+      {
+        source: '/docs/formats/component-story-format',
+        destination: '/docs/api/csf',
+        permanent: true,
+      },
+      {
+        source: '/docs/formats/storiesof-api',
+        destination:
+          'https://github.com/storybookjs/storybook/blob/next/lib/core/docs/storiesOf.md',
+        permanent: true,
+      },
+      {
+        source: '/docs/formats/mdx-syntax',
+        destination: '/docs/api/mdx',
+        permanent: true,
+      },
+      {
+        source: '/docs/testing/react-ui-testing',
+        destination: '/docs/writing-tests',
+        permanent: true,
+      },
+      {
+        source: '/docs/testing/structural-testing',
+        destination: '/docs/writing-tests/snapshot-testing',
+        permanent: true,
+      },
+      {
+        source: '/docs/testing/interaction-testing',
+        destination: '/docs/writing-tests/interaction-testing',
+        permanent: true,
+      },
+      {
+        source: '/docs/testing/automated-visual-testing',
+        destination: '/docs/writing-tests/visual-testing',
+        permanent: true,
+      },
+      {
+        source: '/docs/testing/manual-testing',
+        destination: '/docs/writing-tests',
+        permanent: true,
+      },
+      {
+        source: '/docs/addons/using-addons',
+        destination: '/docs/addons/install-addons',
+        permanent: true,
+      },
+      {
+        source: '/docs/addons/writing-addons',
+        destination: '/docs/addons/writing-addons',
+        permanent: true,
+      },
+      {
+        source: '/addons/writing-addons',
+        destination: '/docs/addons/writing-addons',
+        permanent: true,
+      },
+      {
+        source: '/docs/addons/api',
+        destination: '/docs/addons/addons-api',
+        permanent: true,
+      },
+      {
+        source: '/docs/presets/introduction',
+        destination: '/docs/api/presets',
+        permanent: true,
+      },
+      {
+        source: '/docs/presets/preset-gallery',
+        destination: 'https://github.com/storybookjs/presets',
+        permanent: true,
+      },
+      {
+        source: '/docs/presets/writing-presets',
+        destination: '/docs/api/writing-presets',
         permanent: true,
       },
       /////////////////////////////////////////////////
@@ -126,6 +282,11 @@ module.exports = {
       },
       {
         source: '/testing/:path*',
+        destination: '/docs',
+        permanent: true,
+      },
+      {
+        source: '/docs/guides/:path*',
         destination: '/docs',
         permanent: true,
       },
