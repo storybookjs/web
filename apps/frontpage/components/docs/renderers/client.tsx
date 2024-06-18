d0d0d6b037f597a55023c6795dc20254f1ef4d3e
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-06-18 13:38:13 +0100

    Fix renderers

---
+++ apps/frontpage/components/docs/renderers/client.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { FC, useEffect, useState } from 'react';
+import { useMediaQuery } from '../../../hooks/use-media-query';
+import { renderers } from '@repo/utils';
+import { Button } from './button';
+import { useDocs } from '../../../app/docs/provider';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@repo/ui';
+
+interface RenderersProps {
+  serverActiveRenderer: string;
+}
+
+export const RenderersClient: FC<RenderersProps> = ({
+  serverActiveRenderer,
+}) => {
+  let { activeRenderer, setRenderer } = useDocs();
+  const [isMobile] = useMediaQuery('(max-width: 480px)');
+  const [firstList, setFirstList] = useState(renderers.slice(0, 3));
+  const [lastRenderer, setLastRenderer] = useState(renderers[3]);
+
+  activeRenderer ??= serverActiveRenderer;
+
+  useEffect(() => {
+    // On mobile we only show the first two renderers
+    if (isMobile) {
+      setFirstList(renderers.slice(0, 2));
+      setLastRenderer(renderers[2]);
+    } else {
+      setFirstList(renderers.slice(0, 3));
+      setLastRenderer(renderers[3]);
+    }
+
+    const isInFirstList = firstList.some(
+      (renderer) => renderer.id === activeRenderer,
+    );
+
+    const activeRendererObj = renderers.find(
+      (renderer) => renderer.id === activeRenderer,
+    );
+
+    if (!isInFirstList && activeRendererObj) {
+      setLastRenderer(activeRendererObj);
+    }
+  }, [isMobile]);
+
+  const restRenderers = renderers.filter((r) => {
+    return !firstList.includes(r) && r !== lastRenderer;
+  });
+
+  return (
+    <div className="mb-8 flex gap-2">
+      {firstList.map((renderer) => (
+        <Button
+          active={renderer.id === activeRenderer}
+          key={renderer.id}
+          onClick={() => {
+            setRenderer(renderer.id);
+          }}
+        >
+          {renderer.title}
+        </Button>
+      ))}
+      <Button
+        active={lastRenderer?.id === activeRenderer}
+        onClick={() => {
+          setRenderer(lastRenderer?.id || '');
+        }}
+      >
+        {lastRenderer?.title || ''}
+      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button arrow>More</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="start" collisionPadding={8}>
+          {restRenderers.map((renderer) => (
+            <DropdownMenuItem
+              key={renderer.id}
+              onClick={() => {
+                setRenderer(renderer.id);
+              }}
+            >
+              {renderer.title}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
--- apps/frontpage/components/docs/renderers/index.tsx
+++ apps/frontpage/components/docs/renderers/index.tsx
@@ -1,95 +1,11 @@
-'use client';
+import { type FC } from 'react';
+import { cookies } from 'next/headers';
+import { cookieRenderId } from '../../../constants';
+import { RenderersClient } from './client';
 
-import { renderers } from '@repo/utils';
-import { useEffect, useState, type FC } from 'react';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@repo/ui';
-import { useDocs } from '../../../app/docs/provider';
-import { Button } from './button';
-import { useMediaQuery } from '../../../hooks/use-media-query';
+export const Renderers: FC = () => {
+  const cookieStore = cookies();
+  const activeRenderer = cookieStore.get(cookieRenderId)?.value ?? 'react';
 
-interface RenderersProps {
-  activeRenderer: string;
-}
-
-export const Renderers: FC<RenderersProps> = () => {
-  const { activeRenderer, setRenderer } = useDocs();
-  const [isMobile] = useMediaQuery('(max-width: 480px)');
-  const [firstList, setFirstList] = useState(renderers.slice(0, 3));
-  const [lastRenderer, setLastRenderer] = useState(renderers[3]);
-  const [localRenderer, setLocalRenderer] = useState('react');
-
-  useEffect(() => {
-    if (isMobile) {
-      setFirstList(renderers.slice(0, 2));
-      setLastRenderer(renderers[2]);
-    } else {
-      setFirstList(renderers.slice(0, 3));
-      setLastRenderer(renderers[3]);
-    }
-
-    const isInFirstList = firstList.some(
-      (renderer) => renderer.id === localRenderer,
-    );
-    const activeRendererObj = renderers.find(
-      (renderer) => renderer.id === localRenderer,
-    );
-
-    if (!isInFirstList && activeRendererObj) {
-      setLastRenderer(activeRendererObj);
-    }
-  }, [isMobile, activeRenderer]);
-
-  useEffect(() => {
-    if (activeRenderer) setLocalRenderer(activeRenderer);
-  }, [activeRenderer]);
-
-  const restRenderers = renderers.filter((r) => {
-    return !firstList.includes(r) && r !== lastRenderer;
-  });
-
-  return (
-    <div className="mb-8 flex gap-2">
-      {firstList.map((renderer) => (
-        <Button
-          active={renderer.id === localRenderer}
-          key={renderer.id}
-          onClick={() => {
-            setRenderer(renderer.id);
-          }}
-        >
-          {renderer.title}
-        </Button>
-      ))}
-      <Button
-        active={lastRenderer?.id === localRenderer}
-        onClick={() => {
-          setRenderer(lastRenderer?.id || '');
-        }}
-      >
-        {lastRenderer?.title || ''}
-      </Button>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button arrow>More</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="start" collisionPadding={8}>
-          {restRenderers.map((renderer) => (
-            <DropdownMenuItem
-              key={renderer.id}
-              onClick={() => {
-                setRenderer(renderer.id);
-              }}
-            >
-              {renderer.title}
-            </DropdownMenuItem>
-          ))}
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
-  );
+  return <RenderersClient serverActiveRenderer={activeRenderer} />;
 };
