ed186706af63ef85c59770571d4cd6804801138b
Kyle Gach <kyle.gach@gmail.com>
2024-06-21 14:35:33 -0600

    Remove trailingSlash: true

--- apps/frontpage/next.config.js
+++ apps/frontpage/next.config.js
@@ -67,7 +67,6 @@ module.exports = {
       },
     ],
   },
-  trailingSlash: true,
   // This was added to fix the error with remarkExpressiveCode
   // https://stackoverflow.com/questions/77009138/module-has-no-exports-error-works-fine-on-stackblitz-but-fails-locally
   webpack: (config) => {
