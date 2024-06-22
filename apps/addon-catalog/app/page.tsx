cf8d890bd0419ce7bf02aedb6fed98996cd0eb5d
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-06-22 22:33:39 +0100

    Update page.tsx

--- apps/addon-catalog/app/page.tsx
+++ apps/addon-catalog/app/page.tsx
@@ -2,6 +2,7 @@ import { Preview } from '../components/preview';
 import { buildTagLinks } from '../lib/build-tag-links';
 import { fetchHomeData } from '../lib/fetch-home-data';
 import { TagList } from '../components/tag-list';
+import { PlusIcon, SearchIcon } from '@storybook/icons';
 
 export default async function Home() {
   const {
@@ -13,24 +14,53 @@ export default async function Home() {
 
   const tagLinks = buildTagLinks(trendingTags);
 
+  const categories = [
+    { name: 'Popular', href: '/integrations' },
+    { name: 'Essential', href: '/integrations/tag/essentials' },
+    { name: 'Code', href: '/integrations/tag/essentials' },
+    { name: 'Data & State', href: '/integrations/tag/essentials' },
+    { name: 'Test', href: '/integrations/tag/essentials' },
+    { name: 'Style', href: '/integrations/tag/essentials' },
+    { name: 'Design', href: '/integrations/tag/essentials' },
+    { name: 'Appearance', href: '/integrations/tag/essentials' },
+    { name: 'Organize', href: '/integrations/tag/essentials' },
+  ];
+
   return (
     <>
       <div className="flex items-start justify-between mt-12 mb-8">
         <div>
-          <h1 className="mb-4 text-5xl">Integrations</h1>
+          <h1 className="mb-4 text-4xl font-bold">Integrations</h1>
           <p>
             Integrate your tools with Storybook to connect workflows and unlock
             advanced features.
           </p>
         </div>
-        <button className="flex-shrink-0 h-12 px-6 text-white bg-blue-500 rounded-full text-md">
+        <a
+          href="/docs/react/addons/integration-catalog"
+          className="flex items-center flex-shrink-0 h-10 gap-2 px-5 text-white bg-blue-500 rounded-full text-md"
+        >
+          <PlusIcon />
           Add your integration
-        </button>
+        </a>
       </div>
-      <div className="flex flex-col mb-24 md:flex-row">
-        <div className="w-[220px] flex-shrink-0">Sidebar</div>
+
+      <div className="flex flex-col gap-12 mb-24 md:flex-row">
+        <div className="w-[260px] flex-shrink-0">
+          <div className="flex items-center w-full h-10 gap-2 px-5 border rounded-full border-zinc-300">
+            <SearchIcon /> Search integrations
+          </div>
+          <h3 className="mt-12 mb-8 text-2xl font-bold">Categories</h3>
+          <div className="flex flex-col gap-2 mt-8">
+            {categories.map((category) => (
+              <div key={category.name}>{category.name}</div>
+            ))}
+          </div>
+        </div>
         <div className="flex-1">
-          <TagList tagLinks={tagLinks} />
+          <div className="flex items-center h-10">
+            <TagList tagLinks={tagLinks} />
+          </div>
           <h3 className="mt-12 mb-8 text-2xl font-bold">New to Storybook 8</h3>
           {vta && <Preview key={vta.id} orientation="horizontal" {...vta} />}
           <h3 className="mt-12 mb-8 text-2xl font-bold">Popular addons</h3>
