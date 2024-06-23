5eea690291e4738eb38ce3ccd448a59441a198a2
Kyle Gach <kyle.gach@gmail.com>
2024-06-22 18:13:25 -0600

    Refactor AddonDetail to use route handler for data fetching

---
+++ apps/addon-catalog/app/api/addon/[...name]/route.ts
@@ -0,0 +1,112 @@
+import { ADDON_FRAGMENT, RECIPE_FRAGMENT } from '../../../../constants';
+import { fetchAddonsQuery, gql } from '../../../../lib/fetch-addons-query';
+import { validateResponse } from '../../../../lib/validate-response';
+
+type AddonsData = {
+  addons: Addon[];
+};
+
+type AddonValue = Pick<
+  Addon,
+  | 'name'
+  | 'displayName'
+  | 'description'
+  | 'icon'
+  | 'authors'
+  | 'weeklyDownloads'
+  | 'verified'
+  | 'verifiedCreator'
+  | 'tags'
+  | 'compatibility'
+  | 'status'
+  | 'readme'
+  | 'publishedAt'
+  | 'repositoryUrl'
+  | 'homepageUrl'
+  | 'npmUrl'
+>;
+
+export async function fetchAddonsData(): Promise<AddonValue[] | null> {
+  let value: AddonValue[] | null = null;
+  try {
+    async function fetchPartialData(skip: number = 0): Promise<AddonValue[]> {
+      const data = await fetchAddonsQuery<AddonsData, { skip: number }>(
+        gql`
+          query Addons($skip: Int!) {
+            addons(limit: 30, skip: $skip) {
+              ${ADDON_FRAGMENT}
+              tags {
+                name
+                displayName
+                description
+                icon
+              }
+              compatibility {
+                name
+                displayName
+                icon
+              }
+              status
+              readme
+              publishedAt
+              repositoryUrl
+              homepageUrl
+              npmUrl
+            }
+          }
+        `,
+        {
+          variables: { skip },
+        },
+      );
+
+      validateResponse(() => data?.addons);
+
+      const { addons } = data!;
+
+      value = [...(value || []), ...addons];
+
+      if (addons.length > 0) {
+        await fetchPartialData(skip + addons.length);
+      }
+
+      return value;
+    }
+
+    return await fetchPartialData();
+  } catch (error) {
+    // @ts-expect-error - Seems safe
+    throw new Error(`Failed to fetch addons data: ${error.message}`);
+  }
+}
+
+export async function GET(
+  request: Request,
+  { params }: { params: { name: string[] } },
+) {
+  // TODO: Better decoding?
+  const name = params.name.join('/').replace('%40', '@');
+  
+  let addon: AddonValue | undefined;
+  try {
+    // TODO: Cache this data
+    const addons = (await fetchAddonsData()) || [];
+
+    addon = addons.find((addon) => addon.name === name);
+
+    console.log('>>>>>>>>>>>>>>>>>>', addon);
+
+    if (!addon) {
+      return new Response(`Addon not found: ${name}`, {
+        status: 400,
+      });
+    }
+  } catch (error) {
+    // @ts-expect-error - Seems safe
+    return new Response(`Failed to fetch addons data: ${error.message}`, {
+      status: 400,
+    });
+  }
+
+  return Response.json(addon);
+}
