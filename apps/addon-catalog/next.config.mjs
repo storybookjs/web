0479423e392ea23981c42b0857970e6c97759ead
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-06-23 18:17:55 +0100

    MDX for recipes

--- apps/addon-catalog/next.config.mjs
+++ apps/addon-catalog/next.config.mjs
@@ -1,10 +1,14 @@
+import withMDX from '@next/mdx';
+
 export const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
-const vercelUrl = process.env.VERCEL_URL || process.env.VERCEL_BRANCH_URL
-export const host = vercelUrl ? `https://${vercelUrl}` : 'http://localhost:3001';
+const vercelUrl = process.env.VERCEL_URL || process.env.VERCEL_BRANCH_URL;
+export const host = vercelUrl
+  ? `https://${vercelUrl}`
+  : 'http://localhost:3001';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withMDX()({
   basePath,
   images: {
     remotePatterns: [
@@ -25,6 +29,9 @@ const nextConfig = {
       },
     ],
   },
-};
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+});
 
 export default nextConfig;
