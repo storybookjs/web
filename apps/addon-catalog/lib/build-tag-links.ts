92525bce015e2259e9265b0db71df2b6078d61e6
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-06-23 16:01:33 +0100

    Update build-tag-links.ts

--- apps/addon-catalog/lib/build-tag-links.ts
+++ apps/addon-catalog/lib/build-tag-links.ts
@@ -1,6 +1,8 @@
 export function buildTagLinks(tags: Tag[]): TagLinkType[] {
   return tags.map((tag) => ({
-    name: tag.icon ? `${tag.icon} ${tag.displayName}` : tag.displayName,
+    name: tag.icon
+      ? `${tag.icon} ${tag.displayName}`
+      : tag.displayName || 'Unknown',
     link: `/addons/tag/${tag.name}/`,
   }));
 }
