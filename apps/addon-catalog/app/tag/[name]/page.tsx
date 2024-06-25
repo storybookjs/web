54f16a3401bfd899a8d10aed1d0ffd154ffcaad5
Kyle Gach <kyle.gach@gmail.com>
2024-06-24 21:46:23 -0600

    Remove tags route handler
    - Fetch actual tag details for TagDetails
    - Disable `generateStaticParams` for TagDetails to ensure SSR

--- apps/addon-catalog/app/tag/[name]/page.tsx
+++ apps/addon-catalog/app/tag/[name]/page.tsx
@@ -1,7 +1,6 @@
 import { HomeWrapper } from '../../../components/home-wrapper';
-import { fetchTagsData } from '../../../lib/fetch-tags-data';
-import { fetchSearchData } from '../../../lib/fetch-search-data';
 import { Preview } from '../../../components/preview';
+import { fetchTagDetailsData } from '../../../lib/fetch-tag-details-data';
 
 interface TagDetailsProps {
   params: {
@@ -9,20 +8,21 @@ interface TagDetailsProps {
   };
 }
 
-export async function generateStaticParams() {
-  const categories = (await fetchTagsData({ isCategory: true })) || [];
-  const tags = (await fetchTagsData()) || [];
-  return [...categories, ...tags].map((name) => ({
-    params: { name },
-  }));
-}
+// export async function generateStaticParams() {
+//   const categories = (await fetchTagsData({ isCategory: true })) || [];
+//   const tags = (await fetchTagsData()) || [];
+//   return [...categories, ...tags].map((name) => ({
+//     params: { name },
+//   }));
+// }
 
 export default async function TagDetails({
   params: { name },
 }: TagDetailsProps) {
-  const addons = await fetchSearchData(name);
+  const { topIntegrations } = await fetchTagDetailsData(name) || {};
 
   /**
+   * TODO: Category template
    * You can use `tag.isCategory` to render the correct template
    */
 
@@ -30,7 +30,7 @@ export default async function TagDetails({
     <HomeWrapper>
       <h3 className="mb-8 text-2xl font-bold">{name}</h3>
       <div className="flex flex-col gap-6">
-        {addons?.map((addon) => (
+        {topIntegrations?.addons?.map((addon) => (
           <Preview
             key={addon.name}
             element={addon}
--- apps/addon-catalog/lib/fetch-tag-details-data.ts
+++ apps/addon-catalog/lib/fetch-tag-details-data.ts
@@ -1,18 +1,104 @@
-import { basePath, host } from '../constants';
+import { addonFragment, recipeFragment, validateResponse } from '@repo/utils';
+import { fetchAddonsQuery, gql } from './fetch-addons-query';
 
-export async function fetchTagDetailsData(name: string): Promise<Tag | null> {
-  let tag: Tag | null = null;
+type TagsData = {
+  tags: Tag[];
+};
+
+type TagValue = Pick<
+  Tag,
+  | 'name'
+  | 'displayName'
+  | 'description'
+  | 'icon'
+  | 'relatedTags'
+  | 'topIntegrations'
+>;
+
+async function fetchTagsData({
+  isCategory,
+}: {
+  isCategory?: boolean;
+} = {}): Promise<TagValue[] | null> {
+  let value: TagValue[] | null = null;
   try {
-    const res = await fetch(`${host}${basePath}/api/tag/${name}`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    tag = await res.json();
+    async function fetchPartialData(skip: number = 0): Promise<TagValue[]> {
+      const data = await fetchAddonsQuery<
+        TagsData,
+        { isCategory: boolean; skip: number }
+      >(
+        gql`
+          query Tags($isCategory: Boolean!, $skip: Int!) {
+            tags(isCategory: $isCategory, limit: 30, skip: $skip) {
+              name
+              displayName
+              description
+              icon
+              relatedTags {
+                name
+                displayName
+                icon
+              }
+              topIntegrations(sort: monthlyDownloads) {
+                addons {
+                  ${addonFragment}
+                }
+  
+                recipes {
+                  ${recipeFragment}
+                  addons {
+                    ${addonFragment}
+                  }
+                }
+              }
+            }
+          }
+        `,
+        {
+          variables: { isCategory: Boolean(isCategory), skip },
+        },
+      );
+
+      validateResponse(() => data?.tags);
+
+      const { tags } = data!;
+
+      value = [...(value || []), ...tags];
+
+      if (tags.length > 0) {
+        await fetchPartialData(skip + tags.length);
+      }
+
+      return value;
+    }
+
+    return await fetchPartialData();
   } catch (error) {
     // @ts-expect-error - Seems safe
-    throw new Error(`Failed to fetch tag details data: ${error.message}`);
+    throw new Error(`Failed to fetch tags data: ${error.message}`);
   }
+}
 
-  return tag;
+export async function fetchTagDetailsData(
+  name: string,
+): Promise<(TagValue & { isCategory: boolean }) | null> {
+  try {
+    // TODO: Cache this data
+    const categoriesData = (await fetchTagsData({ isCategory: true })) || [];
+    const tagsData = (await fetchTagsData()) || [];
+
+    const tag = [...categoriesData, ...tagsData].find((tag) => tag.name === name);
+
+    if (!tag) {
+      throw new Error(`Tag not found: ${name}`);
+    }
+
+    return {
+      ...tag,
+      isCategory: categoriesData!.map((category) => category.name).includes(name),
+    };
+  } catch (error) {
+    // @ts-expect-error - Seems safe
+    throw new Error(`Failed to fetch tags data: ${error.message}`);
+  }
 }
