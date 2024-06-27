95465d8d497e5c846800eba842dfe7e1479347f5
Kyle Gach <kyle.gach@gmail.com>
2024-06-27 16:56:41 -0600

    Handle MDX content links on index pages

--- apps/frontpage/components/docs/mdx/a.tsx
+++ apps/frontpage/components/docs/mdx/a.tsx
@@ -1,16 +1,50 @@
+import { DocsVersion, latestVersion } from '@repo/utils';
 import Link from 'next/link';
 import type { FC, ReactNode } from 'react';
 
 interface AProps {
+  activeVersion?: DocsVersion;
   children?: ReactNode;
   href?: string;
+  indexPagePath?: string[] | null;
 }
 
-export const A: FC<AProps> = ({ children, href: hrefIn, ...rest }) => {
-  const href = hrefIn
+/**
+ * ['8.1'] -> 'docs' (when latestVersion is '8.1')
+ * ['8.2'] -> '8.2' (when '8.2' is pre-release)
+ * ['7.6'] -> '7'
+ * [ '8.1', 'configure' ] -> 'configure'
+ * [ '8.1', 'writing-tests', 'snapshot-testing'] -> 'snapshot-testing'
+ */
+function getParentPartOfPath(
+  indexPagePath: string[],
+  activeVersion: DocsVersion,
+) {
+  return indexPagePath.length === 1
+    ? activeVersion.id === latestVersion.id
+      ? 'docs'
+      : activeVersion.inSlug || activeVersion.id
+    : indexPagePath[indexPagePath.length - 1];
+}
+
+export const A: FC<AProps> = ({
+  children,
+  href: hrefIn,
+  indexPagePath,
+  activeVersion = latestVersion,
+  ...rest
+}) => {
+  let href = hrefIn
     ?.replace(/^((?!http).*)\.mdx/, '$1')
     // ../../release-7-6/docs/migration-guide.mdx#major-breaking-changes -> ../../docs/7/migration-guide#major-breaking-changes
     .replace(/^((?!http).*)(?:release-)(\d+)-\d+\/docs(.*)/, '$1docs/$2$3');
+
+  if (indexPagePath && href?.startsWith('./')) {
+    href = href.replace(
+      './',
+      `./${getParentPartOfPath(indexPagePath, activeVersion)}/`,
+    );
+  }
 
   return (
     <a className="ui-text-blue-500" href={href} {...rest}>
