d460a4565c5f2421814e2541d663bea521500359
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-07-02 10:39:31 +0100

    Update docs-versions-redirects.ts

--- apps/frontpage/redirects/docs-versions-redirects.ts
+++ apps/frontpage/redirects/docs-versions-redirects.ts
@@ -18,18 +18,29 @@ export const docsVersionsRedirects: RedirectData[] = [
     destination: `/docs`,
     permanent: true,
   })),
+  // TODO: Not sure about this one - Verify with Kyle
   // The `/get-started` route is only valid for 8.0+
-  ...historicalVersions.reduce<RedirectData[]>((acc, v) => {
-    // Explicitly type the accumulator
-    if (Number(v) < 8) {
-      renderers.forEach((r) => {
-        acc.push({
-          source: `/docs/${v}/${r}/get-started`,
-          destination: `/docs/${v.split('.')[0]}/get-started/install`,
-          permanent: true,
-        });
-      });
-    }
-    return acc;
-  }, []),
+  // ...historicalVersions.reduce<RedirectData[]>((acc, v) => {
+  //   // Explicitly type the accumulator
+  //   if (Number(v) < 8) {
+  //     renderers.forEach((r) => {
+  //       acc.push({
+  //         source: `/docs/${v}/${r}/get-started`,
+  //         destination: `/docs/${v.split('.')[0]}/get-started/install`,
+  //         permanent: true,
+  //       });
+  //     });
+  //   }
+  //   return acc;
+  // }, []),
+  {
+    source: '/docs/6/get-started',
+    destination: '/docs/6',
+    permanent: true,
+  },
+  {
+    source: '/docs/7/get-started',
+    destination: '/docs/7',
+    permanent: true,
+  },
 ];
