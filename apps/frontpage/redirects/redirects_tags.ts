25fb828b75eaa79f16c94b6b97ea79f7f2948e36
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-07-09 18:54:45 +0100

    Revert "Add redirects for tags"

--- apps/frontpage/redirects/redirects_tags.ts
+++
@@ -1,9 +0,0 @@
-import { RedirectData } from './types';
-
-export const redirectsTags: RedirectData[] = [
-  {
-    source: '/tag/:path',
-    destination: '/addons/tag/:path',
-    permanent: true,
-  },
-];
