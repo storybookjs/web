6d499c673fa552e1ac5689104710663a271db55d
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-07-09 20:59:35 +0100

    Bring it all together

---
+++ apps/frontpage/app/showcase/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next';
+import Sitemapper from 'sitemapper';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const blogXml = new Sitemapper({
+    url: 'https://storybook.js.org/showcase/sitemap-0.xml',
+    timeout: 15000, // 15 seconds
+  });
+
+  try {
+    const { sites } = await blogXml.fetch();
+    return sites.map((site) => {
+      const url = site.replace(/\/$/, '');
+      return { url };
+    });
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
