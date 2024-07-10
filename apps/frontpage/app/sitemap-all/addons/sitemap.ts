c580c582e229f3f1edb029f599e93e7c0a4f6172
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-07-10 08:13:48 +0100

    Bring sitemap-all at the root

--- apps/frontpage/app/sitemap-all/addons/sitemap.ts
+++
@@ -1,7 +0,0 @@
-import { MetadataRoute } from 'next';
-import { addonsSitemap } from '../../../lib/get-sitemap-addons';
-
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const urls = await addonsSitemap();
-  return urls;
-}
