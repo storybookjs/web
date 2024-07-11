4504c107e0e7bc24335fc8cf078a1b869b3279fd
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-07-11 10:19:15 +0100

    Fix sitemap docs-all

--- apps/frontpage/app/docs-all/sitemap.ts
+++ apps/frontpage/app/docs-all/sitemap.ts
@@ -1,27 +1,22 @@
 import { MetadataRoute } from 'next';
-import { getDocsTreeFromPath } from '../../lib/get-docs-tree-from-path';
-import { docsVersions } from '@repo/utils';
 import {
   FlatTreeNode,
   getFlatTreeSitemap,
 } from '../../lib/get-flat-tree-sitemap';
 import { getUrl } from '../../lib/get-url';
+import { getAllTrees } from '../../lib/get-all-trees';
+import { docsVersions } from '@repo/utils';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   // Generate docs tree for each version
-  const listofTrees = docsVersions.map((version) => {
-    return {
-      version: version,
-      tree: getDocsTreeFromPath(`content/docs/${version.id}`),
-    };
-  });
+  const listofTrees = getAllTrees();
 
   // We flatten the tree for each version
   const flatTree: FlatTreeNode[] = [];
   listofTrees.forEach((list) => {
-    const newTree = getFlatTreeSitemap(list.tree);
+    const newTree = list.children ? getFlatTreeSitemap(list.children) : [];
     const treeWithVersion = newTree.map((node) => {
-      node.version = list.version;
+      node.version = docsVersions.find((version) => version.id === list.name);
       return node;
     });
 
