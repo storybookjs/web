e46305425f55fa41f1695080ecd38a491cdce866
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-07-19 15:16:56 +0100

    Update page.tsx

--- apps/addon-catalog/app/[...addonName]/page.tsx
+++ apps/addon-catalog/app/[...addonName]/page.tsx
@@ -14,6 +14,8 @@ interface AddonDetailsProps {
   };
 }
 
+export const revalidate = 0;
+
 const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
