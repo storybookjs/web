299f04b53cc743d273008f726bffa31fd2b8beb2
Kyle Gach <kyle.gach@gmail.com>
2024-11-21 22:05:56 -0700

    Add SB Test EAP eyebrow

--- packages/ui/src/header/index.tsx
+++ packages/ui/src/header/index.tsx
@@ -9,7 +9,7 @@ import { StorybookLogo } from '../logos/storybook-logo';
 import { Search } from '../search';
 import { MobileMenu } from './mobile-menu';
 import { Button } from './button';
-// import { Eyebrow } from './eyebrow';
+import { Eyebrow } from './eyebrow';
 import { nav } from './nav';
 
 export interface HeaderProps {
@@ -22,13 +22,13 @@ export interface HeaderProps {
 
 export const Header: FC<HeaderProps> = ({
   algoliaApiKey,
-  eyebrow,
-  // eyebrow = (
-  //   <Eyebrow
-  //     href="https://us02web.zoom.us/webinar/register/WN_XP4uv862TIS1T3SR8voC5Q"
-  //     title="Join live session: Top 8 Storybook myths holding your team back"
-  //   />
-  // ),
+  // eyebrow,
+  eyebrow = (
+    <Eyebrow
+      href="https://storybook.js.org/sb-test-eap"
+      title="Join live session: Test your components in browser with Storybook Test"
+    />
+  ),
   githubCount = 0,
   subMenu,
   variant = 'system',
