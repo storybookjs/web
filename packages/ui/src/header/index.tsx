538d4c2512efc0852024e09f8c09458ad525e449
Kyle Gach <kyle.gach@gmail.com>
2024-12-18 10:41:32 -0700

    Temporarily remove banner from global header

--- packages/ui/src/header/index.tsx
+++ packages/ui/src/header/index.tsx
@@ -9,7 +9,7 @@ import { StorybookLogo } from '../logos/storybook-logo';
 import { Search } from '../search';
 import { MobileMenu } from './mobile-menu';
 import { Button } from './button';
-import { Eyebrow } from './eyebrow';
+// import { Eyebrow } from './eyebrow';
 import { nav } from './nav';
 
 export interface HeaderProps {
@@ -22,13 +22,13 @@ export interface HeaderProps {
 
 export const Header: FC<HeaderProps> = ({
   algoliaApiKey,
-  // eyebrow,
-  eyebrow = (
-    <Eyebrow
-      href="https://storybook.js.org/sb-test-eap"
-      title="Join live session: Test your components in browser with Storybook Test"
-    />
-  ),
+  eyebrow,
+  // eyebrow = (
+  //   <Eyebrow
+  //     href="https://storybook.js.org/sb-test-eap"
+  //     title="Join live session: Test your components in browser with Storybook Test"
+  //   />
+  // ),
   githubCount = 0,
   subMenu,
   variant = 'system',
