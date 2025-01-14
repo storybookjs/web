8709d8e2ee00a7d5c4968cb97f7ea5f1095ed2ea
Kyle Gach <kyle.gach@gmail.com>
2025-01-14 15:08:13 -0700

    8.5 release

--- packages/utils/src/docs-versions.tsx
+++ packages/utils/src/docs-versions.tsx
@@ -28,16 +28,16 @@ export interface DocsVersion {
  **/
 export const docsVersions: DocsVersion[] = [
   {
-    label: 'Version 8.4',
-    id: '8.4',
+    label: 'Version 8.5',
+    id: '8.5',
     branch: 'main',
   },
-  {
-    label: 'Version 8.5 (alpha)',
-    id: '8.5',
-    branch: 'next',
-    preRelease: true,
-  },
+  // {
+  //   label: 'Version 8.6 (alpha)',
+  //   id: '8.6',
+  //   branch: 'next',
+  //   preRelease: true,
+  // },
   {
     label: 'Version 7',
     id: '7.6',
