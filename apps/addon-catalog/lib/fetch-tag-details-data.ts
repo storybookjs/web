ea7d2992498cb77f8d8ac7b2b74e6161d9e8474e
winkerVSbecks <varunvachhar@gmail.com>
2025-05-20 21:00:09 -0400

    switch to tag query to fetch details

--- apps/addon-catalog/lib/fetch-tag-details-data.ts
+++ apps/addon-catalog/lib/fetch-tag-details-data.ts
@@ -12,24 +12,12 @@ type TagValue = Pick<
   | 'topIntegrations'
 >;
 
-interface TagsData {
-  tags: TagValue[];
-}
-
-async function fetchTagsData({
-  isCategory,
-}: {
-  isCategory?: boolean;
-} = {}) {
-  let value: TagValue[] = [];
-  async function fetchPartialData(skip = 0) {
-    const data = await fetchAddonsQuery<
-      TagsData,
-      { isCategory: boolean; skip: number }
-    >(
+async function fetchTagData({ name }: { name: string }) {
+  try {
+    const data = await fetchAddonsQuery<{ tag: TagValue }, { name: string }>(
       gql`
-          query Tags($isCategory: Boolean!, $skip: Int!) {
-            tags(isCategory: $isCategory, limit: 30, skip: $skip) {
+          query Tag($name: String!) {
+            tag(name: $name) {
               name
               displayName
               description
@@ -55,23 +43,15 @@ async function fetchTagsData({
           }
         `,
       {
-        variables: { isCategory: Boolean(isCategory), skip },
+        variables: { name },
       },
     );
 
-    validateResponse(() => data.tags);
+    validateResponse(() => data.tag);
 
-    const { tags } = data;
+    const { tag } = data;
 
-    value = [...value, ...tags];
-
-    if (tags.length > 0) await fetchPartialData(skip + tags.length);
-
-    return value;
-  }
-
-  try {
-    return await fetchPartialData();
+    return tag;
   } catch (error) {
     throw new Error(`Failed to fetch tags data: ${(error as Error).message}`);
   }
@@ -79,20 +59,9 @@ async function fetchTagsData({
 
 export async function fetchTagDetailsData(name: string) {
   try {
-    // TODO: Cache this data
-    const categoriesData = await fetchTagsData({ isCategory: true });
-    const tagsData = await fetchTagsData();
-
-    const tag = [...categoriesData, ...tagsData].find((t) => t.name === name);
-
-    // if (!tag) throw new Error(`Tag not found: ${name}`);
-
+    const tag = await fetchTagData({ name });
     if (!tag) return { error: `Tag not found: ${name}` };
-
-    return {
-      ...tag,
-      isCategory: categoriesData.find((category) => category.name === name),
-    };
+    return tag;
   } catch (error) {
     throw new Error(`Failed to fetch tags data: ${(error as Error).message}`);
   }
