8cfddfa25027e2ad2b7a7da349f8666e6569cc00
Yann Braga <yannbf@gmail.com>
2025-10-13 15:54:25 +0200

    upgrade to Storybook 9

--- apps/frontpage/.storybook/main.ts
+++ apps/frontpage/.storybook/main.ts
@@ -16,9 +16,8 @@ const config: StorybookConfig = {
   ],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath("@storybook/addon-docs")
   ],
   framework: {
     name: getAbsolutePath('@storybook/nextjs'),
--- apps/frontpage/.storybook/preview.ts
+++ apps/frontpage/.storybook/preview.ts
@@ -1,4 +1,4 @@
-import type { Preview } from '@storybook/react';
+import type { Preview } from '@storybook/nextjs';
 import '../app/globals.css';
 import '@repo/ui/styles.css';
 
