c64f03a81bb4df3f3e437b8e1696fefae4ce43e8
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-11-17 16:35:01 +0100

    Add community frameworks component with Nuxt and Analog

--- apps/frontpage/lib/get-page.tsx
+++ apps/frontpage/lib/get-page.tsx
@@ -6,6 +6,7 @@ import type { ReactNode, ReactElement } from 'react';
 import {
   A,
   CodeSnippets,
+  CommunityRenderers,
   FeatureSnippets,
   HomeConcepts,
   HomeRenderers,
@@ -77,6 +78,7 @@ export const getPageData = async (
       CodeSnippets: (props) => (
         <CodeSnippets activeVersion={activeVersion} {...props} />
       ),
+      CommunityRenderers,
       FeatureSnippets,
       FrameworkSupportTable: (props: { children: ReactNode }) => (
         <div {...props}>{props.children}</div>
