095dd0f40efca5ef8c28fe6d8cf48268cdc9adc1
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-11-18 09:54:38 +0100

    chore: Slow down build parallelism to preserve DB

--- apps/addon-catalog/next.config.mjs
+++ apps/addon-catalog/next.config.mjs
@@ -3,6 +3,9 @@ import { withPlausibleProxy } from 'next-plausible';
 
 const nextConfig = withPlausibleProxy()({
   basePath: '/addons',
+  experimental: {
+    cpus: 1,
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
