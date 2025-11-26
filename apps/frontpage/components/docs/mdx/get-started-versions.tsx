af434c675d75323d79514ebad622392b7d993440
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-11-26 18:18:31 +0100

    feat: Make version logos grayscale

--- apps/frontpage/components/docs/mdx/get-started-versions.tsx
+++ apps/frontpage/components/docs/mdx/get-started-versions.tsx
@@ -13,7 +13,7 @@ export function GetStartedVersions({ versions }: GetStartedVersionsProps) {
     <ul className="flex flex-column gap-10 w-full m-0">
       {versions.map(({ name, icon, range }) => (
         <li key={name} className="flex h-12 items-center gap-3 text-md text-black dark:text-white">
-          <Image alt="" height="20" src={icon} width="20" />
+          <Image alt="" height="20" src={icon} width="20" className="grayscale" />
           <p>
             <span className="font-bold">{name}</span> {range}
           </p>
