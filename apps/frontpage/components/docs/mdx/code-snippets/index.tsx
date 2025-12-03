4e8799fafc479ef9fba5c3b628a6e5d90a184ba3
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-12-03 17:04:30 +0100

    Support custom copy events in CodeSnippets

--- apps/frontpage/components/docs/mdx/code-snippets/index.tsx
+++ apps/frontpage/components/docs/mdx/code-snippets/index.tsx
@@ -5,10 +5,22 @@ import { CodeSnippetsClient } from './code-snippets';
 interface LocalProps {
   path?: string;
   activeVersion: DocsVersion;
+
+  /**
+   * An optional event name to send to Plausible when the user copies the code snippet.
+   * Still sends the CodeSnippetCopy event as well. Helps with building funnels as
+   * Plausible doesn't allow filtering events per prop in funnel reports.
+   */
+  copyEvent?: string;
+
+  /**
+   * The new-users variant has much higher contrast on the copy button, to help users
+   * locate it and learn about it. Currently only used for "create" code snippets.
+   */
   variant?: "default" | "new-users";
 }
 
-export async function CodeSnippets({ path, activeVersion, variant = "default" }: LocalProps) {
+export async function CodeSnippets({ path, activeVersion, copyEvent, variant = "default" }: LocalProps) {
   // If there is no path or active version, return null
   // TODO: Perhaps we could return a message saying that there are no code snippets
   if (!path || !activeVersion) return null;
@@ -22,5 +34,5 @@ export async function CodeSnippets({ path, activeVersion, variant = "default" }:
 
   // Render the Code Snippets component
   // This happen on the client since we need to use the context
-  return <CodeSnippetsClient content={codeSnippetsContent} variant={variant} />;
+  return <CodeSnippetsClient content={codeSnippetsContent} variant={variant} copyEvent={copyEvent} snippetPath={path} />;
 }
